6d4434c61df40911882eedb96888e439f3831237
David Sykora <dsykora@gmail.com>
2013-07-01 17:30:52 -0700

    updating package directory for new submodule

--- package.js
+++ package.js
@@ -4,5 +4,5 @@ Package.describe({
 
 Package.on_use(function (api){
   api.use('d3');
-	api.add_files('lib/d3.layout.cloud.js', 'client');
+	api.add_files('d3-cloud/d3.layout.cloud.js', 'client');
 });
